b18225b93b7bea3513a072e95d7e570594a270ad
dramirez648 <dramirez648@gmail.com>
2016-02-29 18:13:19 -0600

    Initial Commit

---
+++ RamirezD_TableViewAppTests/RamirezD_TableViewAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  RamirezD_TableViewAppTests.swift
+//  RamirezD_TableViewAppTests
+//
+//  Created by Daniel Ramirez on 2/29/16.
+//  Copyright © 2016 DePaul University. All rights reserved.
+//
+
+import XCTest
+@testable import RamirezD_TableViewApp
+
+class RamirezD_TableViewAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
